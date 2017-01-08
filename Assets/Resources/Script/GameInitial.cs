a13362bfcd2bcff0bc391f3cc73a9993a8e50961
MrSlip777 <mrslipgamecreate01@gmail.com>
2017-01-08 16:51:55 +0900

    20170108
    create

---
+++ Assets/Resources/Script/GameInitial.cs
@@ -0,0 +1,26 @@
+﻿/*
+*参考URL：http://h-sao.com/blog/2016/05/08/change-unity-window-size/
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitial : MonoBehaviour {
+
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        Screen.SetResolution(1024, 768, false);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Script/SoundTest.cs
@@ -0,0 +1,26 @@
+﻿/*
+*参考URL　http://qiita.com/amano-kiyoyuki/items/43fa92cce1a44a8030b5
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour {
+
+    public AudioClip audioClip1;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip1;
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
